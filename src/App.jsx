037adb53ed5eddd9bcc911d78acd75a9ef1ef571
lirajoaop <joaopedroababa132@gmail.com>
2024-09-23 19:36:33 -0300

    Add sidebar component

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Sidebar } from "./components/Sidebar";
+
+function App() {
+  return (
+    <>
+      <div className="flex mx-auto max-w-7xl ">
+        <Sidebar />
+      </div>
+    </>
+  );
+}
+
+export default App;
