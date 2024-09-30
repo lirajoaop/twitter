127339985183a7d90e6fdcba490ceea80bdfeed4
lirajoaop <joaopedroababa132@gmail.com>
2024-09-30 19:24:27 -0300

    Test mobile touch behavior fix by disabling tap highlight and focus outline

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -7,16 +7,23 @@ import {
   faFeatherAlt,
   faHashtag,
   faHome,
+  faSearch,
   faUser,
   faUserFriends,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const NavItem = ({ icon, text, onClick }) => {
+  const handleInteraction = (e) => {
+    e.preventDefault();
+  };
   return (
     <div
       onClick={onClick}
-      className="flex items-center p-3 rounded-full cursor-pointer hover:bg-gray-600 transition duration-200"
+      onMouseDown={handleInteraction}
+      onTouchStart={handleInteraction}
+      className="flex items-center p-3 rounded-full cursor-pointer hover:bg-gray-600 transition duration-200 focus:outline-none focus-visible:outline-none no-tap-highlight"
+      tabIndex={0}
     >
       <FontAwesomeIcon icon={icon} className="w-6 h-6 mr-4" />
       <span className="text-xl hidden xl:inline">{text}</span>
@@ -35,9 +42,9 @@ export function Sidebar() {
   return (
     <>
       {/*  Horizontal Navbar for Mobile */}
-      <div className="w-full fixed bottom-0 bg-black border-t border-gray-600 xl:hidden flex justify-around">
+      <div className="w-full fixed bottom-0 bg-black border-t border-gray-600 xl:hidden flex justify-around ">
         <NavItem icon={faHome} onClick={handleHomeClick} />
-        <NavItem icon={faHashtag} />
+        <NavItem icon={faSearch} />
         <NavItem icon={faBell} />
         <NavItem icon={faEnvelope} />
       </div>
@@ -50,7 +57,7 @@ export function Sidebar() {
         />
         <nav>
           <NavItem icon={faHome} text="Home" onClick={handleHomeClick} />
-          <NavItem icon={faHashtag} text="Explore" />
+          <NavItem icon={faSearch} text="Explore" />
           <NavItem icon={faBell} text="Notifications" />
           <NavItem icon={faEnvelope} text="Messages" />
           <NavItem icon={faBookmark} text="Bookmarks" />
