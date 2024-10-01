822e55b542882c6c05174de78c5b47d603faf9ac
lirajoaop <joaopedroababa132@gmail.com>
2024-09-30 22:17:09 -0300

    Change interval for new random tweets to 5 seconds

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
   useEffect(() => {
     const interval = setInterval(() => {
       addNewRandomTweets();
-    }, 20000);
+    }, 5000);
     return () => clearInterval(interval);
   }, []);
 
