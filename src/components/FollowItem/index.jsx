64f9b02b908f803e6241bf159287a5bfca4f231f
lirajoaop <joaopedroababa132@gmail.com>
2024-10-02 22:18:12 -0300

    Add toggle functionality for "Follow" and "Following" in FollowItem
    - Implemented a click action to toggle the follow state.
    - The button now switches between "Follow" and "Following" when clicked.
    - Adjusted the button styling to reflect the follow state.

--- src/components/FollowItem/index.jsx
+++ src/components/FollowItem/index.jsx
@@ -1,6 +1,13 @@
 import PropTypes from "prop-types";
+import { useState } from "react";
 
 export function FollowItem({ name, username, avatarUrl }) {
+  const [isFollowing, setIsFollowing] = useState(false);
+
+  const toggleFollow = () => {
+    setIsFollowing((prevState) => !prevState);
+  };
+
   return (
     <div className="flex items-center justify-between py-3 hover:bg-gray-800 transition duration-200">
       <div className="flex items-center">
@@ -14,8 +21,13 @@ export function FollowItem({ name, username, avatarUrl }) {
           <p className="text-gray-500">{username}</p>
         </div>
       </div>
-      <button className="bg-white text-black rounded-full font-bold py-2 px-4 hover:bg-gray-300 transition duration-200">
-        Follow
+      <button
+        onClick={toggleFollow}
+        className={`rounded-full font-bold py-2 px-4 transition duration-200 ${
+          isFollowing ? "bg-black text-white" : "bg-white text-black"
+        }  transition duration-200}`}
+      >
+        {isFollowing ? "Following" : "Follow"}
       </button>
     </div>
   );
